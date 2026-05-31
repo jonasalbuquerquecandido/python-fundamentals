3cf81aae2d8062734cfbcf1c934e64d041f833dd
jonasalbuquerquecandido <jonasalbuquerque.c@gmail.com>
2026-05-31 18:16:08 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+saldo = float(input("Digite o saldo da conta:"))
+nome = str(input("Digite o nome do titular da conta:"))
+status = bool(input("Digite o status da conta:"))
+print("O saldo da conta do cliente", nome, "é de R$:", saldo, "e o status da conta é", status)
+
